ab51646c892f68bd09dea0de6ecfb6c35cd3e15a
Olivia <zoe.i2k1@gmail.com>
2025-02-04 18:14:17 +0100

    The filter changes were reverted

--- src/boson/util/FilterSpec.hpp
+++ src/boson/util/FilterSpec.hpp
@@ -3,7 +3,7 @@
 namespace boson::Filters {
 
 // WARNING: All filters broke on 2025-02-04 following (at the time of writing) unknown changes to the API. 
-constexpr const char* comments = "!-*0E*EUBaf1)"; 
-constexpr const char* postsById = "!)qrxT1HB8VQRNo7I-o1h";
+constexpr const char* comments = "!6WPIompASHLvd"; 
+constexpr const char* postsById = "!nNPvSNOTRz";
 
 }
